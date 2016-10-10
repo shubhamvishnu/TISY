28d9aed7b4fff7bd978825e9ab2afe4d2e0cc497
Shubham Khandelwal <shubhamvishnu@gmail.com>
2016-10-10 20:18:39 +0530

    Implemented different row inflation at different positions in chat interface.

--- app/src/main/java/com/projects/shubhamkhandelwal/tisy/Classes/ChatsRecyclerViewAdpater.java
+++ app/src/main/java/com/projects/shubhamkhandelwal/tisy/Classes/ChatsRecyclerViewAdpater.java
@@ -2,47 +2,38 @@ package com.projects.shubhamkhandelwal.tisy.Classes;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
-import com.google.android.gms.vision.text.Text;
 import com.projects.shubhamkhandelwal.tisy.MapsActivity;
 import com.projects.shubhamkhandelwal.tisy.R;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.zip.Inflater;
 
 /**
  * Created by Shubham Khandelwal on 8/30/2016.
  */
-public class ChatsRecyclerViewAdpater extends RecyclerView.Adapter<ChatsRecyclerViewAdpater.ChatsRecyclerViewHolder> {
-    Context context;
-    private LayoutInflater inflator;
-    public List<EventChat> eventChatList = new ArrayList<>();
-    Firebase firebase;
+public class ChatsRecyclerViewAdpater extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     public static int currentPosition;
+    public static int VIEW_TYPE_USER_MESSAGE = 1;
+    public static int VIEW_TYPE_OTHER_MESSAGE = 2;
+    public List<EventChat> eventChatList = new ArrayList<>();
+    Context context;
+    Firebase firebase;
     LinearLayoutManager layoutManager;
     String currentUsername;
     int chatsReadCount = 0;
+    private LayoutInflater inflator;
 
 
     public ChatsRecyclerViewAdpater(android.content.Context context, LinearLayoutManager linearLayoutManager) {
@@ -55,7 +46,8 @@ public class ChatsRecyclerViewAdpater extends RecyclerView.Adapter<ChatsRecycler
         createSharedPreference();
         setEventChatListener();
     }
-    void createSharedPreference(){
+
+    void createSharedPreference() {
         SharedPreferences sharedPreferences = context.getSharedPreferences(SharedPreferencesName.CHATS_READ_COUNT, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         // to remove all the values from the SharedPreference
@@ -64,11 +56,13 @@ public class ChatsRecyclerViewAdpater extends RecyclerView.Adapter<ChatsRecycler
         editor.apply();
 
     }
-    void updateChatCountSharedPreference(int count){
+
+    void updateChatCountSharedPreference(int count) {
         MapsActivity.numberOfReadChats = count;
         SharedPreferences sharedPreferences = context.getSharedPreferences(SharedPreferencesName.CHATS_READ_COUNT, Context.MODE_PRIVATE);
         sharedPreferences.edit().putInt("chats_read", count).apply();
     }
+
     void setEventChatListener() {
 
         firebase = new Firebase(FirebaseReferences.FIREBASE_ALL_EVENT_DETAILS + Constants.currentEventId + "/chats");
@@ -106,16 +100,37 @@ public class ChatsRecyclerViewAdpater extends RecyclerView.Adapter<ChatsRecycler
     }
 
     @Override
-    public ChatsRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = inflator.inflate(R.layout.recycler_view_chats_row_layout, parent, false);
-        ChatsRecyclerViewHolder viewHolder = new ChatsRecyclerViewHolder(view);
-        return viewHolder;
+    public int getItemViewType(int position) {
+        if (eventChatList.get(position).getUsername().equals(currentUsername)) {
+            return VIEW_TYPE_USER_MESSAGE;
+        } else {
+            return VIEW_TYPE_OTHER_MESSAGE;
+        }
     }
 
     @Override
-    public void onBindViewHolder(ChatsRecyclerViewHolder holder, int position) {
-            holder.usernameTextView.setText(eventChatList.get(position).getUsername());
-            holder.messageTextView.setText(eventChatList.get(position).getMessage());
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_OTHER_MESSAGE) {
+            View view = inflator.inflate(R.layout.recycler_view_chats_other_member_message_recycler_view_row_layout, parent, false);
+            ChatsOtherMemberMessageRecyclerViewHolder viewHolder = new ChatsOtherMemberMessageRecyclerViewHolder(view);
+            return viewHolder;
+        } else {
+            View view = inflator.inflate(R.layout.recycler_view_chats_user_member_message_row_layout, parent, false);
+            ChatsUserMemberMessageRecyclerViewHolder viewHolder = new ChatsUserMemberMessageRecyclerViewHolder(view);
+            return viewHolder;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        String username = eventChatList.get(position).getUsername();
+        String message = eventChatList.get(position).getMessage();
+        if (getItemViewType(position) == VIEW_TYPE_OTHER_MESSAGE) {
+            ((ChatsOtherMemberMessageRecyclerViewHolder) holder).usernameOtherMemberMessageTextView.setText(username);
+            ((ChatsOtherMemberMessageRecyclerViewHolder) holder).messageOtherMemberMessageTextView.setText(message);
+        }else{
+            ((ChatsUserMemberMessageRecyclerViewHolder) holder).messageUserMemberMessageTextView.setText(message);
+        }
     }
 
     @Override
@@ -123,29 +138,23 @@ public class ChatsRecyclerViewAdpater extends RecyclerView.Adapter<ChatsRecycler
         return eventChatList.size();
     }
 
-    class ChatsRecyclerViewHolder extends RecyclerView.ViewHolder {
-
-        TextView usernameTextView;
-        TextView messageTextView;
-        TextView leftSideBorderTextView;
-
-
-        public ChatsRecyclerViewHolder(View itemView) {
-            super(itemView);
-            usernameTextView = (TextView) itemView.findViewById(R.id.username_chat_recycler_view_text_view);
-            messageTextView = (TextView) itemView.findViewById(R.id.message_chat_recycler_view_text_view);
-            leftSideBorderTextView = (TextView) itemView.findViewById(R.id.left_side_message_border_text_view);
-        }
-
-
-    }
-    class ChatsOtherMemberMessageRecyclerViewHolder extends RecyclerView.ViewHolder{
+    class ChatsOtherMemberMessageRecyclerViewHolder extends RecyclerView.ViewHolder {
         TextView usernameOtherMemberMessageTextView;
         TextView messageOtherMemberMessageTextView;
+
         public ChatsOtherMemberMessageRecyclerViewHolder(View itemView) {
             super(itemView);
             usernameOtherMemberMessageTextView = (TextView) itemView.findViewById(R.id.username_other_member_message_text_view);
-            messageOtherMemberMessageTextView  = (TextView) itemView.findViewById(R.id.message_other_member_message_text_view);
+            messageOtherMemberMessageTextView = (TextView) itemView.findViewById(R.id.message_other_member_message_text_view);
+        }
+    }
+
+    class ChatsUserMemberMessageRecyclerViewHolder extends RecyclerView.ViewHolder {
+        TextView messageUserMemberMessageTextView;
+
+        public ChatsUserMemberMessageRecyclerViewHolder(View itemView) {
+            super(itemView);
+            messageUserMemberMessageTextView = (TextView) itemView.findViewById(R.id.message_user_member_message_text_view);
         }
     }
 }
